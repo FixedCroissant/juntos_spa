0341b9499a7e12757ed067c4d940b8fb32492df4
Joshua Williams <jjwill10@ncsu.edu>
2021-03-25 11:45:53 -0400

    chore(parents.php): add associations for parent
    add  associations to pull in other data association with parents

--- app/Models/Parents.php
+++ app/Models/Parents.php
@@ -8,4 +8,24 @@ use Illuminate\Database\Eloquent\Model;
 class Parents extends Model
 {
     use HasFactory;
+    
+    protected $fillable = [
+        'student_id','parent_first_name','parent_last_name','address_line_1','city','state','zip' 
+      ];
+    
+    
+    /**
+     * Inverse of Student/Parent relationship
+     */
+    public function student()
+    {
+        return $this->belongsTo(Student::class,'student_id','id');
+    }
+
+    //Event Attendance
+    public function parentAttendance()
+    {
+        return $this->belongsToMany(Event::class,'event_attendance','event_id','student_id');
+    }
+
 }
