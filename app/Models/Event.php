1f505d72c65f8f5c5f1903696da8c2276b4782b7
Joshua Williams <jjwill10@ncsu.edu>
2021-03-25 11:43:56 -0400

    chore(event.php): add associations for event
    add necessary associations to pull in other data from parent and student attendance of events.

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -9,8 +9,29 @@ class Event extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        'event_start_date','event_end_date','event_type','event_city','event_state','event_zip','event_name','event_notes' 
+      ];
+
+    //Student Attendance  
     public function attendance()
     {
-        return $this->belongsToMany(Attendance::class,'event_attendance_pivot','event_id','attendance_id');
+        return $this->belongsToMany(Student::class,'event_attendance','event_id','student_id')->withTimestamps();
     }
+
+    //Parent Attendance
+    public function parentAttendance()
+    {
+        return $this->belongsToMany(Parents::class,'event_attendance','event_id','parent_id')->withTimestamps();
+    }
+
+    /*
+     * Volunteer attendance.
+     * TO BE CREATED LATER.
+     * return $this->belongsToMany(Volunteer::class,'event_attendance','event_id','volunteer_id')->withTimestamps();
+     */
+
+
+
+
 }
