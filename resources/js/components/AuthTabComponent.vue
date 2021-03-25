012e482fd93e1af3fa2561de839a19324bbd3956
Joshua Williams <jjwill10@ncsu.edu>
2021-03-25 15:22:28 -0400

    feat(frontend admin display): components needed to display the admin area
    included are the necessary components required to display the administrative area.

---
+++ resources/js/components/AuthTabComponent.vue
@@ -0,0 +1,107 @@
+<template>
+  <v-card>
+    <v-toolbar
+      color="grey lighten-1"
+      light
+     >
+      <template v-slot:extension>
+          <!--Selected-->
+        <v-tabs
+          v-model="tab"
+          align-with-title
+          color="black"
+        >
+          <v-tabs-slider color="red"></v-tabs-slider>
+
+          <v-tab
+            v-for="item in items"
+            :key="item"
+            
+          >
+            {{ item }}
+          </v-tab>
+        </v-tabs>
+      </template>
+    </v-toolbar>
+
+    <v-tabs-items v-model="tab">
+      <v-tab-item
+        v-for="item in items"
+        :key="item"
+      >
+        <v-card flat>
+          <v-card-text
+            v-if="item=='Users'"
+          >
+
+          <p>
+            <v-btn
+              elevation="2"
+              small
+             >Create New User
+             </v-btn>
+              <br/>
+              <br/>
+            
+            This is the admin area.
+            
+          </p>
+
+
+          <AdminUserTableComponent users="users"/>
+          </v-card-text>
+          
+          <!--ROLE LIST-->
+          <v-card-text
+          v-if="item=='Roles'"
+          >
+          <br/>
+          <br/>
+          <AdminRoleIndex />
+          </v-card-text>
+          <!--End Roles-->
+          
+          
+          <v-card-text
+            v-if="item=='Notes'"
+          >
+          <ul>
+                    <li>
+                    
+                    </li>
+          </ul>
+          </v-card-text>
+        </v-card>
+      </v-tab-item>
+    </v-tabs-items>
+  </v-card>
+</template>
+
+<script>
+  import AdminUserTableComponent from './AdminUserTableComponent.vue';
+  import AdminRoleIndex from '../views/auth/roles/AdminRoleIndex.vue';
+
+    export default {
+        mounted() {
+            console.log('Auth Tab NoteTab Component mounted.')
+        },
+        components:{
+            AdminUserTableComponent,
+            AdminRoleIndex
+        },
+      props: [],
+      data () {
+      return {
+        tab: null,
+        items: [
+          'Users','Roles', 'District/Counties','Sites','Etc. TBD'
+        ],
+        itemText:[
+          'item1',
+          'item2',
+          'item3'
+        ]        
+      }
+    },
+    }
+</script>
---
+++ resources/js/views/auth/AuthIndex.vue
@@ -0,0 +1,31 @@
+<template>
+   <div>
+
+        <div v-if="$can('read', 'Admin')">
+            <p>
+                    
+
+                    <AuthTabComponent style="padding:100px;"/>
+            </p>
+        </div>
+        <p v-else>
+                404-Page Not Found
+        </p>
+   </div>
+    
+</template>
+
+<script>
+    //Local component imports.
+    import AuthTabComponent from '../../components/AuthTabComponent.vue';
+
+    export default {
+        components:{
+            AuthTabComponent
+        },
+        methods:{            
+        },
+        data:{
+        }  
+    }
+</script>
