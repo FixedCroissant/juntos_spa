b1e2d378eab0c8797973f11c6e85df9fe4a373a0
Joshua Williams <jjwill10@ncsu.edu>
2021-03-25 12:03:55 -0400

    feat(eventcontroller.php): provide additional management of events in backend
    Event management: within the backend handle event management for user.

--- app/Http/Controllers/API/EventController.php
+++ app/Http/Controllers/API/EventController.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Controller;
 
 use App\Models\Event;
 use Illuminate\Http\Request;
+use App\Http\Resources\EventResource;
 
 class EventController extends Controller
 {
@@ -44,7 +45,27 @@ class EventController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $data = $request->all();
+        
+         $validator = \Validator::make($data, [
+             'event_start_date' => 'required|max:255',
+             'event_end_date' => 'required|max:255',
+             'event_name' => 'required',
+             'event_type' => 'required',
+             'event_city' => 'required',
+             'event_state' => 'required',
+             
+         ]);
+
+        //Validation
+         if($validator->fails()){
+             return response(['error' => $validator->errors(), 'Validation Error']);
+         }
+
+        $event = Event::create($data);
+
+        return response([ 'event' => new EventResource($event), 'message' => 'Created Event','status'=>200], 200);
+    
     }
 
     /**
@@ -53,9 +74,23 @@ class EventController extends Controller
      * @param  \App\Models\Event  $event
      * @return \Illuminate\Http\Response
      */
-    public function show(Event $event)
+    public function show($id)
     {
-        //
+        //Attendenace Information as well.
+        $myEvent = Event::find($id);
+
+        //Attendance Information here
+        //TODO REMOVE VALUES THAT ARE NOT NEEDED IN LOOK UP
+        //CURRENTLY PROVIDING WAY TOO MUCH INFORMATION.
+        $attendance=$myEvent->attendance;
+
+        //This is also being used in /events/{id}/attendance/
+
+        //Also get parent attendance.
+        $parentAttendance = $myEvent->parentAttendance;
+
+
+        return response([ 'event' => new EventResource($myEvent), 'message' => 'Retrived Event','status'=>200], 200);
     }
 
     /**
@@ -89,6 +124,47 @@ class EventController extends Controller
      */
     public function destroy(Event $event)
     {
-        //
+        $event->delete();
+
+        return response(['message' => 'Event Deleted'],200);
     }
+
+    
+    /**
+     * Create new Attendance for Student/Parent/Voltunteer Record
+     */
+    public function addAttendance(Request $request, $id){
+        $MyEvent = Event::find($id);
+        //Check Parents...
+        if($request->parents){
+            foreach($request->attendees as $myAttendanceToAdd){
+                //Specifically parents.
+                $attendanceSaved = $MyEvent->parentAttendance()->attach($myAttendanceToAdd);
+            }   
+        }
+        //Students Attendance
+        else if($request->students){
+                     foreach($request->attendees as $myAttendanceToAdd){
+                         $attendanceSaved = $MyEvent->attendance()->attach($myAttendanceToAdd);
+                     }
+         }
+
+        return response([ 'attendance' => $attendanceSaved, 'message' => 'Retrieved successfully'], 200);
+    }
+
+    /**
+     * Remove a {student}/person from attendance
+     */
+    public function removeAttendance(Request $request, $id){
+     
+        //What event are we looking for to add attendance?
+        $MyEvent = Event::find($id);
+       
+       //Remove just one person based on the specific event id; and the "student_id" from the 
+       //event_attendance table.
+       $attendanceSaved = $MyEvent->attendance()->detach($request->attendeesToRemove);
+
+        return response([ 'attendance' => $attendanceSaved, 'message' => 'Retrieved successfully'], 200);
+    }
+
 }
