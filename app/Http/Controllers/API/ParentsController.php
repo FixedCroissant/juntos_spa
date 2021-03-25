e742b24ef47421fb9b69877a1088271d602cb131
Joshua Williams <jjwill10@ncsu.edu>
2021-03-25 11:56:05 -0400

    feat(parentscontroller.php): additional management functionalities for parents
    provided backend resource to properly handle a parent in the application.

--- app/Http/Controllers/API/ParentsController.php
+++ app/Http/Controllers/API/ParentsController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Parents;
 use App\Http\Resources\ParentsResource;
 use Illuminate\Http\Request;
+use DB;
 
 class ParentsController extends Controller
 {
@@ -16,21 +17,16 @@ class ParentsController extends Controller
      */
     public function index()
     {
-        //
-        $parents = Parents::all();
+             $parents = Parents::with(['student' => function($query) {
+                 return $query->select(['id','student_first_name','student_last_name',DB::raw('CONCAT(student_first_name, " ", student_last_name) AS student_full_name')]);
+                 }])->select(
+                    'id',DB::raw('CONCAT(parents.parent_first_name, " ", parents.parent_last_name) AS parent_full_name'),
+                    'city','student_id','address_line_1','city','state','zip',
+                 )->get();
 
         return response([ 'parents' => ParentsResource::collection($parents), 'message' => 'Retrieved successfully'], 200);
     }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+   
 
     /**
      * Store a newly created resource in storage.
@@ -40,7 +36,26 @@ class ParentsController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $data = $request->all();
+
+         $validator = \Validator::make($data, [
+             'student_id' => 'required',
+             'parent_first_name' => 'required|max:255',
+             'parent_last_name' => 'required|max:255',
+             'address_line_1' => 'required',
+             'city' => 'required',
+             'state' => 'required',
+             'zip' => 'required'
+         ]);
+
+        //Validation
+         if($validator->fails()){
+             return response(['error' => $validator->errors(), 'Validation Error']);
+         }
+
+        $parent = Parents::create($data);
+
+        return response([ 'parent' => new ParentsResource($parent), 'message' => 'Created Parent','status'=>200], 200);
     }
 
     /**
@@ -49,32 +64,36 @@ class ParentsController extends Controller
      * @param  \App\Models\Parents  $parents
      * @return \Illuminate\Http\Response
      */
-    public function show(Parents $parents)
+    public function show($id)
     {
-        //
+        //Find Information about our parents.
+        $myParent = Parents::find($id);
+        
+        //->select('parent_first_name','parent_last_name','address_line_1','city','state','zip');
+
+
+        return response([ 'parent' => new ParentsResource($myParent), 'message' => 'Retrived Parent','status'=>200], 200);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Parents  $parents
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Parents $parents)
-    {
-        //
-    }
+    
 
     /**
      * Update the specified resource in storage.
      *
+     * Needs to pass the id of the parent for the proper
+     * update to occur
      * @param  \Illuminate\Http\Request  $request
      * @param  \App\Models\Parents  $parents
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Parents $parents)
+    public function update(Request $request, $parentID)
     {
-        //
+        $myParents = Parents::find($parentID);
+
+        //Update our parent
+        $myParents->update($request->all());
+
+        return response([ 'parents' => new ParentsResource($myParents), 'message' => 'Retrieved successfully'], 200);
     }
 
     /**
@@ -83,8 +102,12 @@ class ParentsController extends Controller
      * @param  \App\Models\Parents  $parents
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Parents $parents)
+    public function destroy(Parents $parents, $id)
     {
-        //
+        $myParent = Parents::find($id);
+
+        $myParent->delete();
+
+        return response(['message' => 'Parent Deleted'],200);
     }
 }
