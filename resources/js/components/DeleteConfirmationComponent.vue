c47b3b2310c8781902c9f1c2ee220cd39a7f3d79
Joshua Williams <jjwill10@ncsu.edu>
2021-03-25 15:07:52 -0400

    chore(deleteconfirmationcomponent): component used for confirmation prompt
    component is currently only used on the parent table, but will be used throughout the application.
    is a prompt to verify confirmation of a decision.

---
+++ resources/js/components/DeleteConfirmationComponent.vue
@@ -0,0 +1,66 @@
+<template>
+  <v-row>
+    <v-dialog
+      v-model="dialog"
+      persistent
+      max-width="350"
+    >
+      <template v-slot:activator="{ on, attrs }">
+        <a
+               v-bind="attrs"
+               v-on="on"
+               
+         >Delete</a>
+      </template>
+      <v-card>
+        <v-card-title class="headline">
+         Really Delete?
+        </v-card-title>
+                    <v-card-text>
+                        Are you really sure that you want to delete this record? 
+                        <br/>
+                        <br/>
+                        There's no going back, so make sure this is something that 
+                        you really want to do.
+                    </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="green darken-1"
+            text
+            @click="dialog = false"
+          >
+            Cancel
+          </v-btn>
+          <v-btn
+            color="green darken-1"
+            text
+            @click="deleteConfirmation(true)"
+          >
+            Please proceed
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-row>
+</template>
+
+<script>
+  export default {
+    props:['recordToRemove'],
+    data () {
+      return {
+        dialog: false,
+      }
+    },
+    methods:{
+        deleteConfirmation(value){
+                //console.log('child component hit... they want to delete something!');
+                 //Send to parent component...
+                //Value they want deleted is passed as a prop.
+                this.$emit('event_deletion', this.$props.recordToRemove);
+                this.$data.dialog=false;
+            },
+    }
+  }
+</script>
