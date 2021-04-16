fefc23c84cfb72641034dcaa52d9d40f0a0b93e8
Joshua Williams <jjwill10@ncsu.edu>
2021-04-16 11:24:26 -0400

    chore: adjust coaching schedule layout
    adjust the schedule layout so that its main student name at top then details below.

--- resources/js/views/coachAppointments/CoachAppointmentIndex.vue
+++ resources/js/views/coachAppointments/CoachAppointmentIndex.vue
@@ -13,64 +13,58 @@
                                 Create New Appointment
                             </v-btn>
                             
-                            <v-simple-table>
-                                        <template v-slot:default>
-                                        <thead>
-                                            <tr>
-                                            <th class="text-left">
-                                                Date Created
-                                            </th>
-                                            <th class="text-left">
-                                               Appointment With: 
-                                            </th>
-                                            <th class="text-left">
-                                                Appointment Date:
-                                            </th>
-                                            
-                                            <th class="text-left">
-                                                Coordinator/Coach:
-                                            </th>
-                                            
-                                            <th class="text-left">
-                                                Review Schedule:
-                                            </th>
+                                                <v-row 
+                                                    v-for="myAppointment in coachAppointment"
+                                                    :key="myAppointment.id">
+                                                    <v-col cols="12" md="12" sm="12">
+                                                        <h3>
+                                                        {{myAppointment.student_full_name}}
+                                                        </h3>
+                                                        <!-- {{myAppointment.coach_appointments}} -->
+                                                        <v-simple-table>
+                                                                <template v-slot:default>
+                                                                              <thead>
+                                                                                <tr>
+                                                                                    <th class="text-left">Student ID</th>
+                                                                                    <th class="text-left">Date Created</th>
+                                                                                    <th class="text-left">Appointment Date</th>
+                                                                                    <th class="text-left">With Coach</th>
+                                                                                    <th class="text-left">Schedule</th>
+                                                                                    <th class="text-left">Actions</th>
+                                                                                </tr>
+                                                                            </thead>
+                                                                            <tbody>
+                                                                                        <tr v-for="myAppointmentInfo in myAppointment.coach_appointments">
+                                                                                            <td>
+                                                                                                   {{myAppointmentInfo.student_id}}
+                                                                                            </td>
+                                                                                            <td>
+                                                                                                {{myAppointmentInfo.created_at | formatDate}}
+                                                                                            </td>
+                                                                                            <td>
+                                                                                                 {{myAppointmentInfo.appointment_date}}
+                                                                                            </td>
+                                                                                            <td>
+                                                                                                     {{myAppointmentInfo.coach.name}}
+                                                                                            </td>
+                                                                                            <td>
+                                                                                              <router-link :to="{name: 'scheduleList', params: { id: myAppointmentInfo.student_id}}">Student Schedule</router-link> 
+                                                                                            </td>
+                                                                                            <td>
+                                                                                                <router-link :to="{name: 'coachAppointmentEdit', params: { id: myAppointmentInfo.id }}">Edit Appointment</router-link>
+                                                                                                |
+                                                                                                <DeleteConfirmationComponent  v-bind:recordToRemove="myAppointmentInfo.id" v-on:event_deletion="checkAppointmentDeletion" />
+                                                                                            </td>
+                                                                                        </tr>
+                                                                            </tbody>
+                                                                </template>
+                                                        </v-simple-table>
 
-                                            <th class="text-left">
-                                                Actions
-                                            </th>
-                                            </tr>
-
-                                             <tr
-                                            v-for="myAppointment in coachAppointment"
-                                            :key="myAppointment.id"
-                                            >
-                                            <td>{{ myAppointment.created_at | formatDate }}</td>
-                                            <td>
-                                                {{myAppointment.student.student_full_name}}
-                                            </td>
-                                            <td>
-                                                {{myAppointment.appointment_date | formatDate }}
-                                            </td>
-                                            <td>
-                                                {{myAppointment.coach.name}}
-                                            </td>
-                                            <td>
-                                                <router-link :to="{name: 'scheduleList', params: { id: myAppointment.student.id}}">Student Schedule List</router-link>
-                                            </td>
-                                            <td>
-                                                <router-link :to="{name: 'coachAppointmentEdit', params: { id: myAppointment.id }}" class="btn btn-primary">Edit Appointment
-                                                </router-link>
-                                                |
-                                                <button class="btn btn-danger">Remove</button>
-              
-                                            </td>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                           
-                                        </tbody>
-                                        </template>
-                                    </v-simple-table>
+                                                                
+                                                    </v-col>
+                                                </v-row>
+                                            
+                                      
                         </template>
                     </v-col>
         </v-row>
@@ -80,11 +74,14 @@
 </template>
 
 <script>
+ import DeleteConfirmationComponent from '../../components/DeleteConfirmationComponent.vue';
     export default {
+        components:{
+                DeleteConfirmationComponent
+        },
         created() {
             this.axios
-                .get(`${process.env.MIX_API_URL}/public/index.php/api/coachAppointment/index`,
-                               
+                .get(`${process.env.MIX_API_URL}/public/index.php/api/coachAppointment/index`,                               
                 {
                   headers:
                   {
@@ -96,12 +93,34 @@
                 });
         },
         methods:{            
-        
-        },
-        computed: {
-             myUserRolesSorted: function () {
-                return this.userRole.sort((a,b)=>(a.id) - (b.id));
+                checkAppointmentDeletion(value){
+                    this.deleteAppointment(value);
+                },
+                repullInformation(){
+                        this.axios
+                            .get(`${process.env.MIX_API_URL}/public/index.php/api/coachAppointment/index`,                               
+                        {
+                        headers:
+                        {
+                            Authorization: `Bearer ${this.$store.getters.token}`,
+                        }
+                        })
+                        .then(response => {
+                                        this.coachAppointment = response.data.appointments;
+                        });
+            },
+                deleteAppointment(id) {
+                this.axios
+                    .delete(`${process.env.MIX_API_URL}/public/index.php/api/coachAppointment/${id}`)
+                    .then(response => {
+                        this.$store.dispatch('setSuccessAlert','Appointment Deleted')
+                                setTimeout(() => {
+                                                this.$store.dispatch('removeSuccessAlert')
+                                }, 5000);
+                    });
+                    this.repullInformation();
             }
+
         },
         data() {
             return {
