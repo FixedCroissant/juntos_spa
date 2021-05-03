444c6b510d360244bc920490956c83c8336ceb8b
Joshua Williams <jjwill10@ncsu.edu>
2021-05-03 09:35:09 -0400

    chore: add nav bars
    add navigation bars for guest and logged in accounts.

---
+++ resources/views/layouts/navbars/navs/auth.blade.php
@@ -0,0 +1,26 @@
+<!-- Navbar -->
+<nav class="navbar navbar-expand-lg navbar-transparent navbar-absolute fixed-top ">
+  <div class="container-fluid">
+    <div class="navbar-wrapper">
+      <a class="navbar-brand" href="#">{{ $titlePage }}</a>
+    </div>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="sr-only">Toggle navigation</span>
+    <span class="navbar-toggler-icon icon-bar"></span>
+    <span class="navbar-toggler-icon icon-bar"></span>
+    <span class="navbar-toggler-icon icon-bar"></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-end">
+      <form class="navbar-form">
+        <div class="input-group no-border">
+    &nbsp;
+        </div>
+      </form>
+      <ul class="navbar-nav">
+          <li class="nav-item">
+              &nbsp;
+          </li>
+      </ul>
+    </div>
+  </div>
+</nav>
---
+++ resources/views/layouts/navbars/navs/guest.blade.php
@@ -0,0 +1,25 @@
+<!-- Navbar -->
+<nav class="navbar navbar-expand-lg navbar-transparent navbar-absolute fixed-top text-black">
+  <div class="container">
+    <div class="navbar-wrapper">
+      <a class="navbar-brand" href="{{ route('home') }}">{{ $title }}</a>
+    </div>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="sr-only">Toggle navigation</span>
+      <span class="navbar-toggler-icon icon-bar"></span>
+      <span class="navbar-toggler-icon icon-bar"></span>
+      <span class="navbar-toggler-icon icon-bar"></span>
+    </button>
+{{--      <div class="collapse navbar-collapse justify-content-end">--}}
+{{--          <ul class="navbar-nav">--}}
+{{--              <li class="nav-item">--}}
+{{--                  <a href="{{route('home')}}">--}}
+{{--                      <i class="material-icons">dashboard</i>--}}
+{{--                      Dashboard--}}
+{{--                  </a>--}}
+{{--              </li>--}}
+{{--          </ul>--}}
+{{--      </div>--}}
+</div>
+</nav>
+<!-- End Navbar -->
