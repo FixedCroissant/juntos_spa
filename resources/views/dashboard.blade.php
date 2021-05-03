9421f17f65253f3c72c4bfe0359dd23f92f9fdeb
Joshua Williams <jjwill10@ncsu.edu>
2021-05-03 09:50:43 -0400

    chore(dashboard): add template for dashboard main page
    template for the dashboard page in application.

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app', ['activePage' => 'dashboard', 'titlePage' => __('')])
+@section('content')
+    <div class="content">
+        <div class="container-fluid">
+            <div class="container-fluid">
+                <div class="card card-plain">
+                    <div class="card-header card-header-primary">
+                        <h4 class="card-title">Dashboard</h4>
+                        <p class="card-category">
+                            &nbsp;
+                        </p>
+                    </div>
+                    <div class="row">
+                        <div class="card">
+                            <div class="col-xs-8 col-sm-8 col-lg-8 offset-1">
+                                &nbsp;
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
