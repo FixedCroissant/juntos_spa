e7f653557556220af4220dffee6362521e7bb2cf
Joshua Williams <jjwill10@ncsu.edu>
2021-05-04 13:44:23 -0400

    refactor(update seeder): update seeder for new field

--- database/seeders/StudentSeeder.php
+++ database/seeders/StudentSeeder.php
@@ -15,13 +15,13 @@ class StudentSeeder extends Seeder
      */
     public function run()
     {
-        //Twenty-five students with 1 parents a piece and 1 default school attached.
-        Student::factory()->count(25)->create(['school_id'=>1])->each(function ($studentRecord){
+        //Twenty-five students with 1 parents a piece and 1 default site attached.
+        Student::factory()->count(25)->create(['site_id'=>1])->each(function ($studentRecord){
                 //Create one parent.
                 $myParent = \App\Models\Parents::factory()->make();
                //Attach to student.
                $studentRecord->parent()->save($myParent);
-              
+
         });
     }
 }
