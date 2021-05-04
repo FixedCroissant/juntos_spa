b8613439ddc1dac5720e4d9947dc363aca0f9531
Joshua Williams <jjwill10@ncsu.edu>
2021-05-04 15:48:46 -0400

    chore: add templated files to be expanded on

---
+++ app/Http/Controllers/ReportingController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Schedule;
+use App\Models\Student;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+use DB;
+
+
+
+class ReportingController extends Controller
+{
+    public function index()
+    {
+
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+
+    }
+
+    public function store(Request $request){
+
+    }
+
+    /**
+     * Edit a new resource.
+     */
+    public function edit($id){
+
+    }
+
+    //Additional methods
+
+    /*
+     * @param $studentID - studentID
+     */
+    public function seeAllStudentsAppointments($studentID){
+
+    }
+
+}
