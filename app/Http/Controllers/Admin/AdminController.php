d5612bcf393ad1df8dcbc8b060183a9e3444f92c
Joshua Williams <jjwill10@ncsu.edu>
2021-05-05 16:34:07 -0400

    chore: necessary methods scaffolded for assigning a user

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -5,6 +5,9 @@ use App\Http\Controllers\Controller;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Models\States;
+use App\Models\County;
+use App\Models\Sites;
 
 class AdminController extends Controller
 {
@@ -26,7 +29,34 @@ class AdminController extends Controller
       * get settings page
       */
     public function settingsList(){
-        // go to our settings page.
         return view('pages.admin.settings.index');
+    }
+    /*
+     * Assign sites to a coordinator.
+     */
+    public function settingsCoordinatorAssignment(Request $request){
+        $states = States::pluck('state_name','id');
+        $countyOptions = County::query();
+        $countyOptions->where('state_id',$request->state_picked);
+        $siteOptions = Sites::query();
+        $siteOptions->where('county_id',$request->county_picked);
+        $assignmentArea = $request->site_picked;
+        $assignmentInformation = $request->has('site_picked');
+        $assignmentDetails = Sites::find($assignmentInformation);
+
+        //User list (ToDo-Narrow down to just coordinators)
+        $userList = User::all();
+
+        return view('pages.admin.settings.coordinator_assignment.index')->with(['userList'=>$userList,'assignmentDetails'=>$assignmentDetails,'assignmentArea'=>$assignmentArea,'states'=>$states,'countyOptions'=>$countyOptions->get(),'siteOptions'=>$siteOptions->get()]);
+    }
+
+    /**
+     *
+     */
+    public function assignedUser(Request $request){
+
+        $userToAssign = $request->assignmentUser;
+
+
     }
 }
