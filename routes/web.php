9b926b6e34689c46b3f59fde691b6ed538696584
Joshua Williams <jjwill10@ncsu.edu>
2021-05-07 12:49:26 -0400

    chore: update route options

--- routes/web.php
+++ routes/web.php
@@ -27,6 +27,7 @@ use App\Http\Controllers\ClassScheduleController;
 use App\Http\Controllers\ReportingController;
 //Student Notes
 use App\Http\Controllers\StudentNotesController;
+use App\Http\Controllers\VolunteerController;
 
 
 Auth::routes();
@@ -42,6 +43,8 @@ Route::group(['middleware' => 'auth'], function () {
 //Students
 //Specific route for event attendance for a student.
 Route::post('/students/attendance',['as'=>'students.addeventattendance','uses'=>'App\Http\Controllers\StudentController@addEventAttendance']);
+Route::post('/students/attendance/complete',['as'=>'students.completeAttendance','uses'=>'App\Http\Controllers\StudentController@addEventAttendnaceComplete']);
+
 
 Route::resource('students',StudentController::class);
 //Adjust parents so that it expects a parameter of our student.
@@ -50,6 +53,10 @@ Route::resource('students',StudentController::class);
 Route::resource('parents',ParentsController::class);
 //Student Notes
 Route::resource('studentnotes',StudentNotesController::class)->except('show','index','show');
+//Specific route for event attendance for a volunteer.
+Route::post('/volunteer/attendance',['as'=>'volunteer.addeventattendance','uses'=>'App\Http\Controllers\VolunteerController@addEventAttendance']);
+
+Route::resource('volunteer',VolunteerController::class)->except('show');
 
 //Event
 Route::resource('event',EventController::class);
@@ -62,7 +69,7 @@ Route::resource('coaching',CoachingAppointmentController::class);
 //Class Schedule
 Route::resource('schedule',ClassScheduleController::class);
 //Reporting Controller
-Route::resource('reporting',ReportingController::class);
+Route::resource('reporting',ReportingController::class)->only(['index','show']);
 
 
 });
@@ -73,10 +80,6 @@ Route::name('admin.')->group(function(){
 Route::get('/admin/index','App\Http\Controllers\Admin\AdminController@index')->name('backend.index');
 //Roles
 Route::get('/admin/roles/index','App\Http\Controllers\Admin\AdminController@roleIndex')->name('backend.rolesindex');
-//States
-//Route::get('/admin/state/index','App\Http\Controllers\Admin\AdminController@statesIndex')->name('admin.backend.statesindex');
-//Use Resource.
-//Not show route.
 Route::resource('/admin/states', StateController::class)->except(['show']);
 //Counties
 Route::resource('/admin/counties', CountyController::class)->except(['show']);
@@ -87,6 +90,15 @@ Route::resource('/admin/users', UserController::class)->except(['show']);
 
 //Misc Actions.//name is admin.settings.index
 Route::get('/admin/settings/index',['uses'=>'App\Http\Controllers\Admin\AdminController@settingsList'])->name('settings.index');
+//Remember admin previx.
+Route::get('/admin/settings/coordinator_assignment',['uses'=>'App\Http\Controllers\Admin\AdminController@settingsCoordinatorAssignment','as'=>'settings.coordinator.assign']);
+
+Route::post('/admin/settings/coordinator_assignment/user',['uses'=>'App\Http\Controllers\Admin\AdminController@assignedUser','as'=>'settings.coordinator.assign.confirm']);
+
+Route::get('/admin/settings/coordinator_assignment/user/index',['uses'=>'App\Http\Controllers\Admin\AdminController@assignmentIndex','as'=>'settings.coordinator.assign.list']);
+
+Route::delete('/admin/settings/coordinator_assignment/user/delete/{userID}/{id}',array('uses' => 'App\Http\Controllers\Admin\AdminController@removeAccess', 'as' => 'settings.coordinator.removeaccess'));
+
 
 
 //ToDo- Add Coordinator Assignment
