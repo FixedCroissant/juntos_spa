8f269ec4eb0c9b02f9346f62d044aa8db108ac6e
Joshua Williams <jjwill10@ncsu.edu>
2021-05-18 15:04:31 -0400

    chore(county.php): fix name for psr-4 autoload

--- app/Models/County.php
+++ app/Models/County.php
@@ -5,14 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class county extends Model
+class County extends Model
 {
     use HasFactory;
 
     protected $fillable = [
-        'state_id','county_name' 
+        'state_id','county_name'
       ];
-    
+
     public function State()
     {
         return $this->belongsTo(States::class,'state_id','id');
