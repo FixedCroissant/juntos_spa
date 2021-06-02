9ae17e39c2e52a286294446f8fab3cdcd4e8bd6c
Joshua Williams <jjwill10@ncsu.edu>
2021-06-02 12:02:39 -0400

    chore: properly take in new filters and clean up exported file
    updated export to see new filtering and made the export a tad nicer.

--- app/Exports/StudentsExport.php
+++ app/Exports/StudentsExport.php
@@ -5,9 +5,40 @@ namespace App\Exports;
 use App\Models\Student;
 use Illuminate\Contracts\View\View;
 use Maatwebsite\Excel\Concerns\FromView;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithEvents;
+use Maatwebsite\Excel\Events\AfterSheet;
+use Maatwebsite\Excel\Concerns\WithTitle;
 
-class StudentsExport implements FromView
+class StudentsExport implements FromView, ShouldAutoSize, WithEvents, WithTitle
 {
+    public $counties;
+    public $site;
+
+    public function __construct($countySearch,$sitePicked){
+        $this->counties = $countySearch;
+        $this->site = $sitePicked;
+    }
+
+    public function title(): string{
+        return 'Students';
+    }
+
+    /**
+     * @return array
+     */
+    public function registerEvents(): array{
+        return [
+            AfterSheet::class=>function (AfterSheet $event){
+            $cellRange = 'A1:S1';
+            $event->sheet->getDelegate()->getStyle($cellRange)->getFont()->setSize(13);
+            $event->sheet->getDelegate()->getStyle($cellRange)->getFont()->setBold(true);
+            $event->sheet->getDelegate()->getStyle($cellRange)->getFill()->setFillType(\PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID)->getStartColor()->setARGB('e6ebe6');
+            },
+        ];
+    }
+
+
     /**
     * @return \Illuminate\Contracts\View\View
     */
@@ -20,8 +51,24 @@ class StudentsExport implements FromView
             $userSites[]=$siteAccess->pivot->site_id;
         }
 
-        $students = Student::leftJoin('sites','students.site_id','=','sites.id')
-            ->whereIn('site_id',$userSites)->get();
+        //Filters
+        $counties = $this->counties;
+        $sitePicked = $this->site;
+
+        //If counties are Null provide everything.
+        if(is_null($counties[0]) && !is_null($sitePicked[0])){
+            $students = Student::leftJoin('sites','students.site_id','=','sites.id')
+                ->whereIn('site_id',$sitePicked)->get();
+        }
+        //No sites and no counties give all.
+        else if(is_null($sitePicked[0]) && is_null($counties[0])){
+            $students = Student::leftJoin('sites','students.site_id','=','sites.id')->get();
+        }
+        else{
+            $students = Student::leftJoin('sites','students.site_id','=','sites.id')
+                ->whereIn('county',$counties)
+                ->whereIn('site_id',$sitePicked)->get();
+        }
 
         return view('pages.reports.students.report_generate', [
             'students'=>$students
