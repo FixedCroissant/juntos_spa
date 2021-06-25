3e514b7914543441cdab3c9dfbaf8b3c27bf86fe
Joshua Williams <jjwill10@ncsu.edu>
2021-06-25 14:47:19 -0400

    refactor: remove extraneous controller no longer needed
    remove old functionality that was no longer being used.

--- routes/web.php
+++ routes/web.php
@@ -49,7 +49,7 @@ Route::get('/', 'App\Http\Controllers\HomeController@index')->name('home')->midd
 //end google signout
 
 Route::group(['middleware' => 'auth'], function () {
-	//Route::resource('user', 'App\Http\Controllers\UserController', ['except' => ['show']]);
+
     Route::get('profile', ['as' => 'profile.edit', 'uses' => 'App\Http\Controllers\ProfileController@edit']);
 	Route::put('profile', ['as' => 'profile.update', 'uses' => 'App\Http\Controllers\ProfileController@update']);
 	Route::put('profile/password', ['as' => 'profile.password', 'uses' => 'App\Http\Controllers\ProfileController@password']);
